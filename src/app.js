d3f16d5da9cfe20b426d94911955f99376b887c7
Montagvonsontag <37384660+Montagvonsontag@users.noreply.github.com>
2025-03-28 13:32:14 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+const arrSuits =['♦','♥','♠','♣'];
+const arrCardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+
+//Functions that give random numbers within the arrays' lenght
+const randomSuits = (arrSuits) => Math.floor(Math.random()*arrSuits.length);
+const randomCardValues = (arrCardValues) => Math.floor(Math.random()*arrCardValues.length);
+
+//Obtain the elements corresponding to the index position within the arrays
+const cardValues = arrCardValues[randomCardValues(arrCardValues)];
+const valueSuits = arrSuits[randomSuits(arrSuits)];
+
+//Selects the HTML elements changing their content
+document.getElementById("valueSuits").textContent = valueSuits;
+document.getElementById("cardValues").textContent = cardValues;
+document.getElementById("suitsDown").textContent = valueSuits;
+
+//timer refreshing the website every 10 seconds (10000 miliseconds)
+setInterval(function(){
+  location.reload();
+}, 10000);
+}
+
+
+
+
